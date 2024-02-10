ab8098f65e88d2f5e0852ba18c41a8cb35eb8280
gowdaganesh005 <gowdaganesh0511@gmail.com>
2024-02-10 23:13:05 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gowdaganesh005/encryption
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func encrpt(key int, str string) string {
+	original := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrestuvwxyz0123456789!@#$%^&*()_+="
+
+}
+
+func main() {
+	var str string
+	fmt.Scanf("%s", &str)
+	encrpyt(key, str)
+
+}
